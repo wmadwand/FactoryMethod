bf30b91242748c987480a730521248d41aaa8bd5
Vasilii Manoilov <wmadwand@gmail.com>
2018-08-20 12:31:18 +0300

    MazeGame, MagicMazeGame classes

---
+++ FactoryMethod/MagicMazeGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace FactoryMethod
+{
+    #region MagicMazeParts
+    public class MagicRoom : Room
+    {
+        private string _spellForOpen;
+
+        public MagicRoom(int number) : base(number)
+        {
+
+        }
+
+        //specific stuff so that make room way more magic
+    }
+
+    public class MagicDoorNeedingSpell : Door
+    {
+        //specific stuff so that make room way more magic
+    }
+    #endregion
+
+    public class MagicMazeGame : MazeGame
+    {
+        public MagicMazeGame()
+        {
+        }
+
+        public override Room MakeRoom(int n)
+        {
+            return new MagicRoom(n);
+        }
+
+        public override Door MakeDoor(Room r1, Room r2)
+        {
+            return new MagicDoorNeedingSpell();
+        }
+    }
+}
---
+++ FactoryMethod/MazeGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace FactoryMethod
+{
+    public class MazeGame
+    {
+        public MazeGame()
+        {
+        }
+
+        public Maze CreateMaze()
+        {
+            Maze aMaze = MakeMaze();
+
+            Room r1 = MakeRoom(1);
+            Room r2 = MakeRoom(2);
+            Door door = MakeDoor(r1, r2);
+
+            aMaze.AddRoom(r1);
+            aMaze.AddRoom(r2);
+
+            //r1.SetSide(Direction.North);
+            //r1.SetSide(Direction.South);
+            //r1.SetSide(Direction.East);
+            //r1.SetSide(Direction.West);
+
+            return aMaze;
+
+        }
+
+        public virtual Maze MakeMaze()
+        {
+            return new Maze();
+        }
+
+        public virtual Room MakeRoom(int n)
+        {
+            return new Room(n);
+        }
+
+        public virtual Wall MakeWall()
+        {
+            return new Wall();
+        }
+
+        public virtual Door MakeDoor(Room r1, Room r2)
+        {
+            return new Door();
+        }
+    }
+}
